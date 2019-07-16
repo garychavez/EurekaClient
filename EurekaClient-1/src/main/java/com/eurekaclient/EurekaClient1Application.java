3577b9d4b2a81dba700f2dbb4acf8924d96d796d
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-16 17:15:42 -0500

    16-07

--- EurekaClient-1/src/main/java/com/eurekaclient/EurekaClient1Application.java
+++ EurekaClient-1/src/main/java/com/eurekaclient/EurekaClient1Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClient1Application {
@@ -11,5 +13,5 @@ public class EurekaClient1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClient1Application.class, args);
 	}
-
+	
 }
